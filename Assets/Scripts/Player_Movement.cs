6c5b554904edfd46fd027755867663a98e4cd5c6
jinxuanq <ethanqjx@gmail.com>
2025-09-27 15:09:13 -0400

    PlayerMovement+collider

---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 5f; // movement speed
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        moveInput = new Vector2(moveX, moveY).normalized;
+
+    }
+    void FixedUpdate()
+    {
+        // Move using physics
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+    }
+
+}
