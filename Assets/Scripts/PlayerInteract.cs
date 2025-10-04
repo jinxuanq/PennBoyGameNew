2512ef73457bd8aeb15abce6eb608e10618669d8
Brendan Zankowski <brendiesel5@gmail.com>
2025-10-04 03:29:19 -0400

    Counter
    tweaked temporary visuals and player size for clarity, created a counter prefab and player rascasting for interacting with objects

---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+
+    private Vector3 lastInteractInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInteractions();
+    }
+
+    private void HandleInteractions()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+        if (moveInput != Vector2.zero)
+        {
+            lastInteractInput = moveInput;
+        }
+        float raycastDistance = 1f;
+        RaycastHit2D raycastHit = Physics2D.Raycast(transform.position, lastInteractInput, raycastDistance);
+        if (raycastHit)
+        {
+            //Debug.Log(raycastHit.transform);
+        }
+        else
+        {
+            //Debug.Log("-");
+        }
+    }
+}
