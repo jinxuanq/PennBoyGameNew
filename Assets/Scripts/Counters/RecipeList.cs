b0a1d3e2a9aa4a356de71628d471fe81b49bedb6
jinxuanq <ethanqjx@gmail.com>
2025-11-04 17:43:44 -0500

    listed reciepes

---
+++ Assets/Scripts/Counters/RecipeList.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class RecipeList : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public RecipeDataBase recipeDatabase;
+    public TextMeshProUGUI recipeListText;
+    void OnEnable()
+    {
+        UpdateRecipeList();
+    }
+
+    void UpdateRecipeList()
+    {
+        string text = "<b>Recipes</b>\n";
+
+        foreach (var recipe in recipeDatabase.allRecipes)
+        {
+            text += $"\n<b>{recipe.drinkName}</b>\n";
+
+            foreach (var ing in recipe.requiredIngredients)
+            {
+                text += $"• {ing}\n";
+            }
+        }
+
+        recipeListText.text = text;
+    }
+}
